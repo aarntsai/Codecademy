b02b6e65ca7effd2b88ee67fc8090440f7a09da4
corgiwizard <33399608+corgiwizard@users.noreply.github.com>
2020-07-31 13:17:48 -0500

    exercises and such

---
+++ C++ Exercises/C++ Exercises.cpp
@@ -0,0 +1,17 @@
+// C++ Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+    std::cout << "please enter x variable \n";
+    std::cin >> x;
+    std::cout << "please enter y variable \n";
+    std::cin >> y;
+    int sum = x + y;
+    std::cout << "variable x plus variable y is " << sum << "\n";
+}
+
